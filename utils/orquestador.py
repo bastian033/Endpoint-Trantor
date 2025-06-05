852d3317a1434b8d3ebdfac128eb9673990c65cd
BastianMadrid <ba.madrid@duocuc.cl>
2025-06-05 16:24:14 -0400

    quitando carpetas y archivos 2.0

--- utils/orquestador.py
+++ utils/orquestador.py
@@ -5,7 +5,7 @@ def ejecutar(nombre, comando):
     print(f"\n--- Ejecutando: {nombre} ---")
     resultado = subprocess.run(comando)
     if resultado.returncode != 0:
-        print(f"Error ejecutando {nombre}, Deteniendo orquestador.")
+        print(f"Error ejecutando {nombre}, deteniendo orquestador")
         sys.exit(1)
     print(f"--- {nombre} finalizado correctamente ---\n")
 
@@ -13,14 +13,15 @@ if __name__ == "__main__":
 
     # Primero para los datos de DatosGob
 
-    # 1. para ejecutar el scraper
     ejecutar("Scraper DatosGob", ["python", "scrapers/scraper_datosgob.py"])
-    
-    # 2. para ejecutar la migracion 
+
     ejecutar("Migracion DatosGob", ["python", "utils/migracionDG.py"])
     
-    # 3. para ejecutar la limpieza
-    ejecutar("Limpieza DatosGob", ["python", "utils/limpiezaSII.py"]) 
+    # Ahora para los datos de SII
 
+    ejecutar("Migracion SII", ["python", "scrapers/scraper_sii.py"])
+
+    ejecutar("Migracion SII", ["python", "utils/migracionSII.py"])
+ 
     
     print("Orquestador DatosGob: Proceso completo.")
